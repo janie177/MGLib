388e55eda27d1fe27be75e1331f9f90732b32010
Jan <janie177@hotmail.com>
2016-06-28 05:44:42 +0200

    Timed bossbars now are removed instantly.

--- src/main/java/net/minegusta/mglib/bossbars/TimedBossBarHolder.java
+++ src/main/java/net/minegusta/mglib/bossbars/TimedBossBarHolder.java
@@ -44,7 +44,7 @@ public class TimedBossBarHolder extends BossBarHolderModel {
 				{
 					removeAllPlayers();
 					BossBarManager.removeBar(this);
-				}, duration + 10);
+				}, duration);
 
 	}
 }
