a5c091de05d6fb002b57ff7e1f4a6b7c39a460e9
Jan <janie177@hotmail.com>
2016-06-28 03:05:30 +0200

    Added an a new type of abstract particle. Checks for block and entity collision. Moves statically.

---
+++ src/main/java/net/minegusta/mglib/particles/MovingParticleWithAction.java
@@ -0,0 +1,66 @@
+package net.minegusta.mglib.particles;
+
+import com.google.common.collect.Lists;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+
+import java.util.List;
+
+public abstract class MovingParticleWithAction extends ParticleEffect {
+
+	private List<Entity> targets = Lists.newArrayList();
+	private int interval = 20;
+	private int counter = 0;
+	private boolean blocks;
+
+	public MovingParticleWithAction(int duration, Effect effect, Location location, double blocksPerSecond, Location target, boolean blocks) {
+		super(duration, effect, location, blocksPerSecond, target);
+		this.blocks = blocks;
+		scanForEntities();
+	}
+
+	@Override
+	public void extra() {
+
+		//Check for hits
+		if(counter % 2 == 0)
+		{
+			if(blocks && !location.getBlock().getType().isTransparent())
+			{
+				end();
+				return;
+			}
+			for(Entity ent : targets)
+			{
+				if(ent.getLocation().distance(location) < 1)
+				{
+					end();
+					break;
+				}
+			}
+		}
+
+
+		counter++;
+		if(counter >= interval)
+		{
+			scanForEntities();
+			counter = 0;
+		}
+	}
+
+	private void end()
+	{
+		onHit();
+		ParticleRegistry.remove(this);
+	}
+
+	private void scanForEntities()
+	{
+		targets.clear();
+		location.getWorld().getLivingEntities().stream().filter(ent -> ent.getLocation().distance(location) < 20).forEach(targets::add);
+	}
+
+	public abstract void onHit();
+}
